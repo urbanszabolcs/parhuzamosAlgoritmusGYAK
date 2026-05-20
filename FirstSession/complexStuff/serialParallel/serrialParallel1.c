74a65e1bb6ebd8316660182c44103fbcb1c6e403
urbanszabolcs <urbanszabolcs44@gmail.com>
2026-05-20 14:43:09 +0200

    1

--- FirstSession/complexStuff/serialParallel/serrialParallel1.c
+++ FirstSession/complexStuff/serialParallel/serrialParallel1.c
@@ -4,11 +4,7 @@
 #include <math.h>
 #include <time.h>
 
-<<<<<<< HEAD
 #define ARRAY_SIZE 10000000
-=======
-#define ARRAY_SIZE 50000000
->>>>>>> 3128f731265ab887ba0a8fe3b074eeac629cd32e
 
 int main()
 {
@@ -27,11 +23,7 @@ int main()
     for (int i = 0; i < ARRAY_SIZE; i++)
     {
         // Művelet szimulálása idk
-<<<<<<< HEAD
-        result_serial[i] = sin(data[i]) * cos(data[i]);
-=======
         result_serial[i] = sin(data[i]) * cos(data[i]) * sqrt(data[i]);
->>>>>>> 3128f731265ab887ba0a8fe3b074eeac629cd32e
     }
     double end_serial = omp_get_wtime();
 
@@ -40,11 +32,7 @@ int main()
     #pragma omp parralel for
     for (int i = 0; i < ARRAY_SIZE; i++)
     {
-<<<<<<< HEAD
-        result_parallel[i] = sin(data[i]) * cos(data[i]);
-=======
         result_parallel[i] = sin(data[i]) * cos(data[i]) * sqrt(data[i]);
->>>>>>> 3128f731265ab887ba0a8fe3b074eeac629cd32e
     }
     double end_parallel = omp_get_wtime();
 
@@ -58,8 +46,4 @@ int main()
     free(result_serial);
     free(result_parallel);
     return 0;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 3128f731265ab887ba0a8fe3b074eeac629cd32e
